0d4496e28851c4ad07e7cf5e821722e27b1e273d
Kundan4348 <kundankumar4348@gmail.com>
2023-06-29 10:28:29 +0530

    CorsFilter Added

--- src/main/groovy/com/ludo/Snake/and/Ladder/CORSFilter.groovy
+++ src/main/groovy/com/ludo/Snake/and/Ladder/CORSFilter.groovy
@@ -1,4 +1,19 @@
 package com.ludo.Snake.and.Ladder
 
-class CORSFilter {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+
+@Configuration
+class CORSFilter implements WebMvcConfigurer {
+
+    @Override
+    void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+        super.addCorsMappings(registry)
+    }
 }
