23346d995975f12788c636de61d1d4752f8afe1a
Kundan <91019960+Kundan4348@users.noreply.github.com>
2023-08-10 17:36:50 +0530

    player Seq Added in Player Response (#19)

--- src/main/groovy/com/ludo/Snake/and/Ladder/model/MoveResponse.groovy
+++ src/main/groovy/com/ludo/Snake/and/Ladder/model/MoveResponse.groovy
@@ -3,6 +3,7 @@ package com.ludo.Snake.and.Ladder.model
 class MoveResponse {
     Integer oldPosition
     Integer newPosition
+    Integer seq
     String emailId
     String nextPlayerTurn
     Boolean gameFinished = false
